331e7c9c3fc710d875d764769847bfa2e5ed11f3
David Quintero <davidq@gmail.com>
2020-10-31 16:00:43 -0400

    initial functionality achieved

--- routes/convert.js
+++ routes/convert.js
@@ -18,8 +18,38 @@ router.get("/", (req, res) => {
   let unit = array.slice(index[0]).join("");
   let value = array.slice(0, index[0]).join("");
 
-  console.log(input, index, value, unit);
-  res.send(input);
+  const measures = {
+    // [relative unit, name, converts to]
+    km: [0.62137, "kilometers", "mi", "miles"],
+    mi: [1.60934, "miles", "km", "kilometers"],
+    kg: [2.20462, "kilograms", "lbs", "pounds"],
+    lbs: [0.45359, "pounds", "kg", "kilograms"],
+    gal: [3.78541, "gallons", "l", "liters"],
+    l: [0.26417, "liters", "gal", "gallons"],
+  };
+
+  const msg =
+    value +
+    " " +
+    measures[unit][1] +
+    " " +
+    "converts to" +
+    " " +
+    measures[unit][0] * value +
+    " " +
+    measures[unit][3];
+
+  const output = {
+    initNum: value,
+    initUnit: unit,
+    returnNum: measures[unit][0] * value,
+    returnUnit: measures[unit][2],
+    string: msg,
+  };
+
+  console.log(output);
+
+  res.send(output);
 });
 
 module.exports = router;
