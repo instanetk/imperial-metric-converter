b6ac03d1fb6c496d070281e8a0399a74b00f180d
David Quintero <davidq@gmail.com>
2020-10-30 22:19:46 -0400

    split query into value and unit

--- routes/convert.js
+++ routes/convert.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.get("/", (req, res) => {
   const input = req.query.input;
-  console.log(input);
+  const array = input.split("");
+  let index = [];
+
+  // Find beginning of unit in input string
+  for (char of array) {
+    let test = /[a-z]/;
+    if (char.match(test)) {
+      index.push(array.indexOf(char));
+    }
+  }
+
+  // Define unit and value individually
+  let unit = array.slice(index[0]).join("");
+  let value = array.slice(0, index[0]).join("");
+
+  console.log(input, index, value, unit);
   res.send(input);
 });
 
